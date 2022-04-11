a611f5b873f863fdb9e06c18597aa6a1649d7177
SEC556 <71392712+SEC556@users.noreply.github.com>
2022-04-11 18:32:01 -0400

    Create update-labs-h01.sh

---
+++ update-labs-h01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+{ # this ensures the entire script is downloaded #
+pushd . >/dev/null
+#cd /root/sec617/bluetooth/btfind/
+#git pull >/dev/null
+popd >/dev/null
+
+echo "Nothing to update."
+
+} # this ensures the entire script is downloaded #
